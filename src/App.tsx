8f64f687e36a7d9ed2888b9d0cf72ea85234f460
Anukul Pandey <anukul.030601@gmail.com>
2024-02-28 19:27:25 +0530

    feat: maintenance page

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled, { css,keyframes } from "styled-components";
+import Navbar from './Navbar';
+
+const breakpoints = {
+	xs: "480px",
+	sm: "768px",
+	md: "992px",
+	lg: "1200px",
+	xl: "1400px",
+};
+
+
+export const respondTo = Object.keys(breakpoints).reduce(
+	(accumulator, label) => {
+    // @ts-ignore
+    (accumulator as any)[label] = (...args:any) => css`@media (max-width: ${breakpoints[label]}) {${css(...args)};
+			}
+		`;
+		return accumulator;
+	},
+	{}
+);
+
+const bgGradientAnimation = keyframes`
+	0%{
+		background-size: 300%;
+		background-position: 0%;
+	}
+	50%{
+		background-size: 150%;
+		background-position: 100%;
+	}
+	100%{
+		background-size: 300%;
+		background-position: 0%;
+	}
+`;
+
+const Wrapper = styled.div`
+	position: relative;
+	height: 40rem;
+	width: 100%;
+	background: var(--app-banner-bg);
+	background-repeat: repeat;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	line-height: 1.125;
+	gap: 2rem;
+	min-height: 100%;
+	text-align: left;
+	overflow: hidden;
+	h1 {
+		line-height: 1;
+		font-weight: 900;
+		font-size: 4rem;
+		background: -webkit-linear-gradient(
+			0deg,
+			var(--app-text) 0%,
+			#619feb 50%,
+			var(--app-text) 100%
+		);
+		background-size: 300%;
+		background-repeat: no-repeat;
+		background-clip: text;
+		-webkit-background-clip: text;
+		width: 25rem;
+		-webkit-text-fill-color: transparent;
+		/* animation: ${bgGradientAnimation} 10s ease infinite; */
+		user-select: none;
+	}
+	h2 {
+		margin-top: 2rem;
+		font-weight: 500;
+		font-size: 1.125rem;
+		width: 25rem;
+		color: var(--app-container-text-primary-hover);
+	}
+	${(respondTo as any).md`
+	    display: grid;
+		padding-left: 0;
+		background: var(--app-banner-bg-sm);
+		h1{
+			width: 100%;
+			font-size: 3.5rem;
+		}
+		h2{
+			font-size: 1.125rem;
+			margin-right:0;
+			width: 100%;
+		}
+	`}
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Navbar />
+    <Wrapper>
+				<div style={{margin: "0 2rem"}}>
+					<h1>An ocean of possibilities</h1>
+					<h2>Sqwid seeks to create a safe and accessible environment, one in which artists can utilize the Reef blockchain to distribute their work in novel ways and forge closer bonds with their followers.</h2>
+				</div>
+			</Wrapper>
+      </Wrapper>
+  )
+}
+
+export default App
---
+++ src/Navbar.tsx
@@ -0,0 +1,98 @@
+import React, { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+	position: fixed;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	font-weight: 700;
+	font-size: 1.25rem;
+	padding: ${props => ((props as any).blur ? `1.25rem 1.5rem` : `2.5rem 3.75rem`)};
+	backdrop-filter: ${props =>
+		(props as any).blur ? `blur(5px) brightness(0.375) saturate(0.25)` : `none`};
+	z-index: 50;
+	top: 0;
+	/* border-bottom: ${props =>
+		(props as any).blur ? `1px` : `0`} solid var(--app-container-bg-primary); */
+	transition: backdrop-filter 0.2s ease, padding 0.2s ease;
+	&:after {
+		position: absolute;
+		content: "";
+		bottom: 0;
+		left: 0;
+		background: var(--app-container-bg-primary);
+		height: 1px;
+		width: 100%;
+		display: block;
+		opacity: ${props => ((props as any).blur ? `1` : `0`)};
+		z-index: -1;
+	}
+`;
+
+const LogoContainer = styled.a`
+	cursor: pointer;
+	user-select: none;
+	display: inline-block;
+	align-items: center;
+	font-size: 2rem;
+	font-weight: 900;
+	text-decoration: none;
+	color: var(--app-text);
+	span,
+	svg {
+		vertical-align: middle;
+	}
+	span {
+		padding-left: 0.5rem;
+	}
+`;
+
+const SVG = styled.svg`
+	background: transparent;
+	fill: currentColor;
+`;
+
+const LogoIcon = () => {
+	return (
+		<SVG
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 100 100"
+		>
+			<path d="M50 7C29.554 7 13 23.554 13 44v3c0 3.217 1.358 5.972 3.219 7.875C18.079 56.778 20.417 58 23 58h8v13c0 2.652-1.59 4-4 4-1.284 0-2.549-.911-3.344-2.406a3.006 3.006 0 1 0-5.312 2.812C19.954 78.436 23.057 81 27 81c5.345 0 10-4.342 10-10V58h4v23c0 2.652-1.59 4-4 4-1.153 0-2.234-.463-2.844-1.094a3 3 0 1 0-4.312 4.156C31.749 90.031 34.367 91 37 91c5.345 0 10-4.342 10-10V58h6v23c0 5.658 4.655 10 10 10 2.633 0 5.251-.969 7.156-2.938a3 3 0 1 0-4.312-4.156C65.234 84.536 64.154 85 63 85c-2.41 0-4-1.348-4-4V58h4v13c0 5.658 4.655 10 10 10 3.943 0 7.046-2.564 8.656-5.594a3.006 3.006 0 1 0-5.312-2.812C75.549 74.089 74.284 75 73 75c-2.41 0-4-1.348-4-4V58h8c2.583 0 4.92-1.222 6.781-3.125A11.265 11.265 0 0 0 87 47v-3C87 23.554 70.446 7 50 7zm0 6c17.226 0 31 13.774 31 31v3c0 1.533-.642 2.747-1.531 3.656C78.579 51.566 77.417 52 77 52H23c-.417 0-1.58-.434-2.469-1.344C19.642 49.746 19 48.533 19 47v-3c0-17.226 13.774-31 31-31zM40 33c-4.38 0-8 3.62-8 8a3 3 0 1 0 6 0c0-1.16.84-2 2-2 1.16 0 2 .84 2 2a3 3 0 1 0 6 0c0-4.38-3.62-8-8-8zm20 0c-4.38 0-8 3.62-8 8a3 3 0 1 0 6 0c0-1.16.84-2 2-2 1.16 0 2 .84 2 2a3 3 0 1 0 6 0c0-4.38-3.62-8-8-8z" />
+		</SVG>
+	);
+};
+
+const Navbar = React.memo(() => {
+	const [isAtTop, setIsAtTop] = useState(true);
+	const offsetLimit = 20;
+	const checkIsAtTop = useCallback(
+		() => {
+			isAtTop === true && setIsAtTop(false);
+			window.pageYOffset <= offsetLimit && setIsAtTop(true);
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[]
+	);
+	useEffect(() => {
+		window.addEventListener("scroll", checkIsAtTop);
+		return () => window.removeEventListener("scroll", checkIsAtTop);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isAtTop]);
+	return (
+		// @ts-ignore
+		<Nav blur={isAtTop}>
+			<LogoContainer href="/" className="animate-icon">
+				<LogoIcon />
+				<span>Sqwid</span>
+			</LogoContainer>
+		<div style={{fontWeight:'lighter',color:'yellow'}}>🚨 Marketplace currently down for maintenance</div>	
+		</Nav>
+	);
+});
+
+export default Navbar;
